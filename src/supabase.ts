b945fe357ceb4e2ae3207c977699182287f1af4b
DeiberV <deiberveranodev@gmail.com>
2024-11-06 19:11:17 -0500

    fix

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://lsvnkghztdjfxsxmuvjo.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxzdm5rZ2h6dGRqZnhzeG11dmpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5MzU3MDMsImV4cCI6MjA0NjUxMTcwM30.2ahfp3HmsNsWQCnaImQ_MJS6OrRcaawc77FphgiHhF0',
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SECRET,
 );
